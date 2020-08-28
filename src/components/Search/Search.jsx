0a75e02c8809175f1f112b850c00ca340036864a
Adam D <fadehelix@gmail.com>
2020-08-28 16:02:26 +0200

    Move data fetching function into separate file

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { setResults, setCurrentPhrase } from 'data/redux/search/search.actions';
-import ky from 'ky';
+
+import API from 'data/api';
+
 import style from './Search.module.scss';
 
 function Search() {
   const dispatch = useDispatch();
-  const apiUrl = process.env.REACT_APP_API_URL;
 
   const handleSearch = async (event) => {
     dispatch(setCurrentPhrase(event.target.value));
-
-    const search = await ky
-      .get(`${apiUrl}location/search/`, {
-        searchParams: { query: event.target.value },
-        retry: 0,
-      })
-      .json();
-    dispatch(setResults(search));
+    const res = await API.searchLocations(event.target.value);
+    dispatch(setResults(res));
   };
 
   return (
---
+++ src/data/api.js
@@ -0,0 +1,18 @@
+import ky from 'ky';
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+const searchLocations = (phrase) => {
+  return ky
+    .get(`${apiUrl}location/search/`, {
+      searchParams: { query: phrase },
+      retry: 0,
+    })
+    .json();
+};
+
+const API = {
+  searchLocations,
+};
+
+export default API;
