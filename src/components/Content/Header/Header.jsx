40f195f7131334558230b33b86a01f8aea883549
Adam D <fadehelix@gmail.com>
2020-09-03 12:54:43 +0200

    Display today date in header

--- src/components/Content/Header/Header.jsx
+++ src/components/Content/Header/Header.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Header({ locationDetails }) {
+  const pattern = /(\d{4})-(\d{2})-(\d{2})/g;
+  const formattedDate =
+    locationDetails && locationDetails.time.match(pattern)[0];
   return (
     <header>
       {locationDetails ? (
         <>
           <h1>{locationDetails.title}</h1>
-          <p>Check date</p>
+          <p>{formattedDate}</p>
         </>
       ) : (
         <h1>Choose city</h1>
